b8fdae7caf85577e57ff2f333107a57fc4918469
Dagotra <s.sannikov@turbocontract.ru>
2026-02-07 15:09:42 +0500

    fixed

--- test_main.py
+++ test_main.py
@@ -5,21 +5,30 @@ from faker import Faker
 
 fake = Faker()
 URL = "https://store.steampowered.com/"
+UNIQ_ELEMENT_MAIN_PAGE = By.XPATH, '//*[@id="home_maincap_v7"]'
 ENTRANCE_BUTTON = By.XPATH, "//*[@id='global_actions']//a[contains(@href, 'login')]"
 OBJECT_LOGIN = By.XPATH, "//div[@data-featuretarget='login']"
-INPUT_LOGIN = By.XPATH, "//div[@data-featuretarget='login']//input[@type='text']"
-INPUT_PASSWORD = By.XPATH, "//div[@data-featuretarget='login']//input[@type='password']"
-LOGIN_BUTTON = By.XPATH, "//div[@data-featuretarget='login']//button[@type='submit']"
-LOADER = By.XPATH, "//div[@data-featuretarget='login']//button[@type='submit']//div//div"
-ERROR_TEXT = By.XPATH, "(//div[@data-featuretarget='login']//div)[20]"
+INPUT_LOGIN = By.XPATH, f"{OBJECT_LOGIN[1]}//input[@type='text']"
+INPUT_PASSWORD = By.XPATH, f"{OBJECT_LOGIN[1]}//input[@type='password']"
+LOGIN_BUTTON = By.XPATH, f"{OBJECT_LOGIN[1]}//button[@type='submit']"
+LOADER = By.XPATH, f"{OBJECT_LOGIN[1]}//button[@type='submit']//div//div"
+ERROR_TEXT = By.XPATH, f"({OBJECT_LOGIN[1]}//div)[20]"
 STRING_ERROR_TEXT = "Пожалуйста, проверьте свой пароль и имя аккаунта и попробуйте снова."
+TIMEOUT_WEBDRIVER = 10
 
 
 def test_error_login_steam(browser):
-    wait = WebDriverWait(browser, 10)
+    wait = WebDriverWait(browser, TIMEOUT_WEBDRIVER)
 
     browser.get(URL)
-    assert URL in browser.current_url, "Это не главная страница"
+
+    wait.until(
+        EC.visibility_of_element_located(UNIQ_ELEMENT_MAIN_PAGE),
+        message=(
+            f"Уникальный элемент страницы '{UNIQ_ELEMENT_MAIN_PAGE[1]}' "
+            f"не найден за '{TIMEOUT_WEBDRIVER}' секунд."
+        )
+    )
 
     wait.until(EC.element_to_be_clickable(ENTRANCE_BUTTON)).click()
     wait.until(EC.visibility_of_element_located(OBJECT_LOGIN))
@@ -36,4 +45,5 @@ def test_error_login_steam(browser):
     error_text = wait.until(EC.visibility_of_element_located(ERROR_TEXT))
     text = error_text.text
     assert STRING_ERROR_TEXT == text, \
-        f"Текст ошибки при невалидной авторизации не совпадает с шаблоном текста '{STRING_ERROR_TEXT}'"
+        (f"Неверный текст ошибки. Ожидаемый результат: '{text}'."
+         f"Фактический результат: '{STRING_ERROR_TEXT}'")
