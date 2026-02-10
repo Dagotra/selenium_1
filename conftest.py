c0504c763e01c18bc95d0de515696a1dcddfc706
Dagotra <s.sannikov@turbocontract.ru>
2026-02-10 15:05:34 +0500

    change fixture scope

--- conftest.py
+++ conftest.py
@@ -2,19 +2,11 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-from selenium_1.steam_site.home_page import HomePage
 
-
-@pytest.fixture(scope="session")
+@pytest.fixture
 def browser():
     options = Options()
     options.add_argument("--start-maximized")
     driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
-
-@pytest.fixture
-def home_page(browser):
-    page = HomePage(browser)
-    page.open_page()
-    return page
