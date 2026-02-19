e984ebbd028c534e6fda837069fb57ce9aa10382
Dagotra <s.sannikov@turbocontract.ru>
2026-02-19 19:16:34 +0500

    optimizing  conftest

--- conftest.py
+++ conftest.py
@@ -4,12 +4,6 @@ from selenium_1.driver_singleton import DriverSingleton
 
 @pytest.fixture
 def browser(request):
-    driver = DriverSingleton()
-    lange = request.param
-    valid_language = (driver.options.to_capabilities().get('goog:chromeOptions').get('args'))[1]
-    if lange != valid_language[7:]:
-        DriverSingleton.close_driver()
-        driver = DriverSingleton(language=request.param)
+    driver = DriverSingleton(language=request.param)
     yield driver
-
     DriverSingleton.close_driver()
