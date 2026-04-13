615613a5953b31ac58a81914f3587f6d0dd27bae
Dagotra <s.sannikov@turbocontract.ru>
2026-04-13 22:24:38 +0500

    added chrome in dockerfile

--- browser/browser_factory.py
+++ browser/browser_factory.py
@@ -1,3 +1,4 @@
+import os
 from enum import StrEnum
 from selenium import webdriver
 from selenium.webdriver.remote.webdriver import WebDriver
@@ -19,6 +20,10 @@ class BrowserFactory:
         headless = config.get("browser", "headless")
         if headless:
             options.add_argument("--headless=new")
+        if os.path.exists("/.dockerenv"):
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+            options.binary_location = "/usr/bin/chromium"
         Logger.info(f"Создаем вебдрайвер '{driver_name}' c опциями '{options.arguments}'")
         driver = webdriver.Chrome(options=options)
         return driver
