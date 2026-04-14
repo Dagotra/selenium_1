d726ad7e41a05756408f06996802e5d5e366cb69
Dagotra <s.sannikov@turbocontract.ru>
2026-04-14 12:16:45 +0500

    change BaseElement.method scroll_into_view -> native: wait_for_presence

--- elements/base_element.py
+++ elements/base_element.py
@@ -135,5 +135,5 @@ class BaseElement:
             raise
 
     def scroll_into_view(self) -> None:
-        native = self.wait_for_visible()
+        native = self.wait_for_presence()
         self.browser.execute_script("arguments[0].scrollIntoView();", native)
