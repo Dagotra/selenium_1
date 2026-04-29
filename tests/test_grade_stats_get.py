dc89796f711bb42446cbc7b9587a0b3588096034
Dagotra <s.sannikov@turbocontract.ru>
2026-04-29 23:50:05 +0500

    added assert in tests

--- tests/test_grade_stats_get.py
+++ tests/test_grade_stats_get.py
@@ -8,9 +8,9 @@ class TestGradeStatsGet:
         university_service = UniversityService(api_utils=university_api_utils_admin)
         teacher = create_and_delete_grade.teacher_id
 
-        stats = GradeStatsRequest(student_id=teacher)
+        stats = GradeStatsRequest(teacher_id=teacher)
         stats_response = university_service.get_grade_stats(grade_stats=stats)
-        print(stats_response.count)
+        assert stats_response.max is not None or stats_response.min is not None, "The query filter returns empty values"
 
     def test_grade_stats_student(self, university_api_utils_admin, create_and_delete_grade: GradeResponse):
         university_service = UniversityService(api_utils=university_api_utils_admin)
@@ -18,9 +18,10 @@ class TestGradeStatsGet:
 
         stats = GradeStatsRequest(student_id=student)
         stats_response = university_service.get_grade_stats(grade_stats=stats)
-        print(stats_response.count)
+        assert stats_response.max is not None or stats_response.min is not None, "The query filter returns empty values"
 
     def test_general_grade_stats(self, university_api_utils_admin, create_and_delete_grade: GradeResponse):
         university_service = UniversityService(api_utils=university_api_utils_admin)
         general_stats = GradeStatsRequest()
         stats_response = university_service.get_grade_stats(general_stats)
+        assert stats_response.max is not None or stats_response.min is not None, "The query filter returns empty values"
